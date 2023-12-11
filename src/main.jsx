84b970d5d84b6fe48b993153f65df6fb79d3336b
alanguarino <alaan.guarino@gmail.com>
2023-12-11 12:07:55 -0300

    added workflow GH Actions

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,12 @@ import "./index.css";
 import { ThemeProvider } from "@emotion/react";
 import theme from "../src/utils/theme";
 import { CssBaseline } from "@mui/material";
-// import { BrowseRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      {/* <Router> */}
       <CssBaseline />
       <App />
-      {/* </Router> */}
     </ThemeProvider>
   </React.StrictMode>
 );
